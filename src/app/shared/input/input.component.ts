8e192ce1382a442b405404a34852c1e166606aa0
Victor <adsvictor@outlook.com>
2019-02-08 14:09:08 -0200

    Add Reactive Forms

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { NgModel, FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'mt-input-container',
@@ -12,6 +12,7 @@ export class InputComponent implements OnInit, AfterContentInit {
   @Input() label: string
 
   @ContentChild(NgModel) model: NgModel
+  @ContentChild(FormControlName) control: FormControlName
 
   constructor() { }
 
@@ -19,9 +20,9 @@ export class InputComponent implements OnInit, AfterContentInit {
   }
 
   ngAfterContentInit() {
-    this.input = this.model
+    this.input = this.model || this.control
     if (this.input === undefined) {
-      throw new Error('This component must be used with ngModel')
+      throw new Error('This component must be used with ngModel or formControlName')
     }
   }
 
